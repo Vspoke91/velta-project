ba4063c46b05cddc814f69e81cff3067158e49a6
Vspoke91 <victorvjro91@gmail.com>
2023-01-31 15:36:05 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-logo" href='index'>
+          <span>Velta Projects</span>
+        </a>
+        <ul>
+          <li><a href="index">Code</a></li>
+          <li><a href="index">Digital Art</a></li>
+          <li><a href="index">My Life</a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
