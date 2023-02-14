66a143e13a15e4db3c0360e76d04d2162d19c38d
Vspoke91 <victorvjro91@gmail.com>
2023-02-14 14:28:43 -0500

    ADD: Other Pages have been link to a URL

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import '../styles/App.css';
-import HomePage from '../pages/Home'
-import CodingPage from '../pages/Coding'
+import HomePage from '../pages/Home';
+import CodingPage from '../pages/Coding';
+import ArtWorkPage from '../pages/ArtWork';
 import ErrorPage from '../pages/ErrorPage';
 
 function App() {
@@ -10,8 +11,16 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <CodingPage/>,
+      element: <HomePage/>,
       errorElement: <ErrorPage />,
+    },
+    {
+      path: "/Coding",
+      element: <CodingPage/>,
+    },
+    {
+      path: "/ArtWork",
+      element: <ArtWorkPage/>,
     },
   ]);
 
