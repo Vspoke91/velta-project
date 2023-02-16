a17a09f88c8edb63d06ddd22a8b01196ee441c0c
Vspoke91 <victorvjro91@gmail.com>
2023-02-16 11:45:41 -0500

    ADD: added style and basic layout for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Header from '../components/Header';
+import '../styles/Home.css'
 
 function Home() {
   return (
     <divc className='main'>
       <Header logo_Name={'Velta Projects'}/>
+      <div className='mainContent'>
+        <div className='contentTittle'>
+          <h1>Velta Projects</h1>
+        </div> {/*This will be tittle with a background of all my work like (20. Alexis Johnson Art) https://www.sitebuilderreport.com/inspiration/modern-websites?a=ga&keyword=dsa-363651420386&ad=624613568096&gclid=Cj0KCQiAxbefBhDfARIsAL4XLRrRf1BHkaaZKXrPIml5i91turSWLEtz4bpT5Tb4m4uW8Pvckt60dq8aAn-ZEALw_wcB */}
+        <div className='contentCoding'>Coding Section</div>
+        <div className='contentArtWork'>Art-Work Section</div>
+        <div className='contentMyLife'>My life section</div>
+      </div>
     </divc>
   )
 }
