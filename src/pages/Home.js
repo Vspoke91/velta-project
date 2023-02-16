d35fd0346a0d77978c407f61fe0c6f64814b9a44
Vspoke91 <victorvjro91@gmail.com>
2023-02-16 17:07:26 -0500

    ADD: content for coding and art work has being added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,16 +11,26 @@ function Home() {
           <h1>Velta Projects</h1>
         </div> {/*This will be tittle with a background of all my work like (20. Alexis Johnson Art) https://www.sitebuilderreport.com/inspiration/modern-websites?a=ga&keyword=dsa-363651420386&ad=624613568096&gclid=Cj0KCQiAxbefBhDfARIsAL4XLRrRf1BHkaaZKXrPIml5i91turSWLEtz4bpT5Tb4m4uW8Pvckt60dq8aAn-ZEALw_wcB */}
         
-        <div className='contentCoding'>
-          <div className='codingText'>
+        <div className='contentItem' style={{backgroundImage: "url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png)"}}>
+          <div className='itemText'>
             <h1>Coding</h1>
             <h2>I have being coding since I was 16 years old in middle School</h2>
           </div>
-          <div className='codingImages'>
-            <h1>Imgae</h1>
+          <div className='itemImages'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png'/>
           </div>
         </div>
-        <div className='contentArtWork'>Art-Work Section</div>
+
+        <div className='contentItem invertFlex' style={{backgroundImage: "url(https://media.istockphoto.com/id/825383494/photo/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-problems-concept.jpg?s=612x612&w=0&k=20&c=wtqvbQ6OIHitRVDPTtoT_1HKUAOgyqa7YzzTMXqGRaQ=)"}}>
+          <div className='itemText'>
+              <h1>Art Work</h1>
+              <h2>My digital art is something I have done the same mount as my coding</h2>
+            </div>
+            <div className='itemImages'>
+              <img src='https://media.istockphoto.com/id/825383494/photo/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-problems-concept.jpg?s=612x612&w=0&k=20&c=wtqvbQ6OIHitRVDPTtoT_1HKUAOgyqa7YzzTMXqGRaQ='/>
+            </div>
+        </div>
+        
         <div className='contentMyLife'>My life section</div>
       </div>
     </divc>
