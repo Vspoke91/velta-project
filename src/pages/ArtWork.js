14d2359f2a6267379550ba07b458ff4b1656ae2e
Vspoke91 <victorvjro91@gmail.com>
2023-02-20 13:42:23 -0500

    ADD: style for art work route
    CHNG: art work now display all artwork in images

--- src/pages/ArtWork.js
+++ src/pages/ArtWork.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import '../styles/ArtWork.css'
 import Header from '../components/Header';
 
+const images = importAll(require.context('/public/images/ArtWork/', false, /\.(png|jpe?g|svg)$/));
+
+function importAll(r) {
+  let images = [];
+  r.keys().forEach((item, index) => { images[index] = r(item); });
+  return images
+}
+
 function ArtWork() {
+
+  let imageElement = []
+
+  Object.values(images).forEach((img, index) => {imageElement[index] = <li><img src={img} alt = {img}/></li>});
+
   return (
+  
     <div className='main'>
       <Header logo_Name={'Velta Projects'}/>
+      <div className='mainContent'>
+        <ul>
+          {imageElement}
+        </ul>
+      </div>
     </div>
   )
 }
