784789175376b6fb1f4b965d697e38050790dfc0
Vspoke91 <victorvjro91@gmail.com>
2023-02-21 15:59:54 -0500

    CHNG: make code more compact

--- src/pages/Coding.js
+++ src/pages/Coding.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import '../styles/Coding.css'
 import Header from '../components/Header';
-import Content from '../components/Content';
 import SideBarNavigation from '../components/SideBarNavigation';
 import ContentData from '../data/CodingProjectsData'
+import ContentObject from '../components/ContentObject'
 
 function Coding() {
+
+  let contentElements = new Array(ContentData.length);
+
+  for(let i = 0; i < ContentData.length; i++){
+    contentElements[i] = <ContentObject
+    Tittle = {ContentData[i].tittle} 
+    Description = {ContentData[i].description}
+    Pictures = {ContentData[i].images}
+    Skills = {ContentData[i].skills}
+    />;
+  }
+
   return (
     <div className='main'>
       <Header logo_Name={'Velta Projects'} />
-      <div>
+      <div className='mainContent mainContentCoding'>
         <SideBarNavigation contentItems={ContentData}/>
-        <Content contentItems={ContentData}/>
+        <div className='contentItems'>
+          {contentElements}
+        </div>
       </div>
     </div>
   )
